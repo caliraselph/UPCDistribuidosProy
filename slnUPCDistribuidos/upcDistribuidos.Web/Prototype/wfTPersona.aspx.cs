536e45ca081424519f4f6aeac4f42b9d9641dee4
brayan.bonifaz <brayan.bonifaz@gmail.com>
2016-10-28 02:08:07 -0500

    dw

---
+++ slnUPCDistribuidos/upcDistribuidos.Web/Prototype/wfTPersona.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace upcDistribuidos.Web.Views
+{
+    public partial class wfTPersona : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnNew2_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("wfBPersona.aspx");
+        }
+    }
+}
