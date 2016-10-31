b1af51f612ca661d569a001074c4a88cb8671ab9
Carlos Castillo <ccastilloestrada@gmail.com>
2016-10-30 22:13:54 -0500

    Actualizacion Carlos.

---
+++ slnUPCDistribuidos/upcDistribuidos.Web/Prototype/wfBPersona.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace upcDistribuidos.Web.Prototype
+{
+    public partial class wfBPersona : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            grdPersona.DataSource = dt;
+        }
+
+        protected void btnBuscar_Click(object sender, ImageClickEventArgs e)
+        {
+
+            DataTable dt = grdPersona.DataSource as DataTable;
+            dt.Columns.Add("Codigo");
+            dt.Columns.Add("Apellidos");
+            dt.Columns.Add("Nombres");
+            dt.Columns.Add("Tipo");
+            dt.Columns.Add("NroLibros");
+            dt.Columns.Add("Deuda");
+            dt.Columns.Add("Telefono");
+            dt.Columns.Add("Email");
+
+            DataRow row = dt.NewRow();
+
+
+            row["Codigo"] = "U2015";
+            row["Apellidos"] = "CASTILLO ESTRADA";
+            row["Nombres"] = "CARLOS EMILIO";
+            row["Tipo"] = "ALUMNO";
+            row["NroLibros"] = "1";
+            row["Deuda"] = "0.00";
+            row["Telefono"] = "45756345";
+            row["Email"] = "ccastillo@hotmail.com";
+
+            dt.Rows.Add(row);
+
+            grdPersona.DataSource = dt;
+            grdPersona.DataBind();
+        }
+
+        protected void btnSalir_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("wfPrincipal.aspx");
+        }
+
+        protected void btnNuevo_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("wfTPersona.aspx");
+        }
+    }
+}
---
+++ slnUPCDistribuidos/upcDistribuidos.Web/Prototype/wfBPersona.aspx.designer.cs
@@ -0,0 +1,105 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace upcDistribuidos.Web.Prototype {
+    
+    
+    public partial class wfBPersona {
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// Control btnNuevo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton btnNuevo;
+        
+        /// <summary>
+        /// Control btnBuscar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton btnBuscar;
+        
+        /// <summary>
+        /// Control btnSalir.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton btnSalir;
+        
+        /// <summary>
+        /// Control Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+        
+        /// <summary>
+        /// Control txtCodigo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCodigo;
+        
+        /// <summary>
+        /// Control Label3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+        
+        /// <summary>
+        /// Control txtDescripcion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDescripcion;
+        
+        /// <summary>
+        /// Control grdPersona.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView grdPersona;
+    }
+}
